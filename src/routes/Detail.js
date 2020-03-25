3eb0bf3e3288e0fa9a0250a60340dfa0929f08b2
bluecandle <jinyeongkim37@gmail.com>
2020-03-25 23:54:46 +0900

    chapter #1 done.

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -10,19 +11,91 @@ const GET_MOVIE = gql`
       title
       medium_cover_image
       description_intro
+      language
+      rating
+    }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
     }
   }
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+  background-image: url(${props => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+`;
+
+
 export default () => {
+// 아래 코드를 사용하면 url 을 구성하는 부분 중에서 ~~.com/:id/~~ 이런 형태일 때, id 라는 param 으로 지정되어 있는 값을 획득할 수 있다.
   const { id } = useParams();
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id }
   });
-  if (loading) {
-    return "loading";
-  }
-  if (data && data.movie) {
-    return data.movie.title;
-  }
+//   if (loading) {
+//     return "loading";
+//   }
+//   if (data && data.movie) {
+//     return data.movie.title;
+//   }
+return (
+    <Container>
+      <Column>
+        {/* <Title>Name</Title>
+        <Subtitle>English · 4.5</Subtitle>
+        <Description>lorem ipsum lalalla </Description> */}
+        <Title>{loading ? "Loading..." : data.movie.title}</Title>
+        {/* {!loading && data.movie && (
+          <>
+            <Subtitle>
+              {data.movie.language} · {data.movie.rating}
+            </Subtitle>
+            <Description>{data.movie.description_intro}</Description>
+          </>
+        )} */}
+        <Subtitle>
+          {data?.movie?.language} · {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro}</Description>
+      </Column>
+      {/* <Poster
+        bg={data && data.movie ? data.movie.medium_cover_image : ""} */}
+      {/* ></Poster> */}
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
+    </Container>
+  );
 };
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
+import {gql} from "apollo-boost";
+import {useQuery} from "@apollo/react-hooks";
 import styled from "styled-components";
 import Movie from "../components/Movie";
 
-
-const Container = styled.div`
+const Container = styled.div `
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
 `;
 
-const Header = styled.header`
+const Header = styled.header `
   background-image: linear-gradient(-45deg, #d754ab, #fd723a);
   height: 45vh;
   color: white;
@@ -23,25 +22,33 @@ const Header = styled.header`
   width: 100%;
 `;
 
-const Title = styled.h1`
+const Title = styled.h1 `
   font-size: 60px;
   font-weight: 600;
   margin-bottom: 20px;
 `;
 
-const Subtitle = styled.h3`
+const Subtitle = styled.h3 `
   font-size: 35px;
 `;
 
-const Loading = styled.div`
+const Loading = styled.div `
   font-size: 18px;
   opacity: 0.5;
   font-weight: 500;
   margin-top: 10px;
 `;
 
+const Movies = styled.div `
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
 
-const GET_MOVIES = gql`
+const GET_MOVIES = gql `
   {
     movies {
       id
@@ -50,22 +57,27 @@ const GET_MOVIES = gql`
   }
 `;
 
-export default () => {
-  const { loading, data } = useQuery(GET_MOVIES);
-//   const { loading, error, data } = useQuery(GET_MOVIES);
-  return (
-    <Container>
-      <Header>
-        <Title>Apollo 2020</Title>
-        <Subtitle>I love GraphQL</Subtitle>
-      </Header>
-      {/* 이 코드가 loading 상태인 경우 Loading 화면을 보여주게 하는 코드! */}
-      {/* {loading && <Loading>Loading...</Loading>}  */}
-
-      {/* 이런식으로 && 를 여러개 붙이면, 앞에 적힌 두 조건이 true 인 경우 마지막(세번째) 코드를 실행하라는 의미이다 */}
-      {!loading &&
-        data.movies &&
-        data.movies.map(m => <Movie key={m.id} id={m.id} />)}
-    </Container>
-  );
+export default() => {
+    const {loading, data} = useQuery(GET_MOVIES);
+    return (
+        <Container>
+            <Header>
+                <Title>Apollo 2020</Title>
+                <Subtitle>I love GraphQL</Subtitle>
+            </Header>
+            {loading && <Loading>Loading...</Loading>}
+            {/* {!loading && data.movies && (
+                <Movies>
+                    {data
+                        .movies
+                        .map(m => (<Movie key={m.id} id={m.id} bg={m.medium_cover_image}/>))}
+                </Movies>
+            )} */}
+            <Movies>
+                {data
+                    ?.movies
+                        ?.map(m => (<Movie key={m.id} id={m.id} bg={m.medium_cover_image}/>))}
+            </Movies>
+        </Container>
+    )
 };
