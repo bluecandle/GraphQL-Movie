0e4a3f77ff4897904fb10e5a865250d68c87b5ca
bluecandle <jinyeongkim37@gmail.com>
2020-03-25 22:50:53 +0900

    Apollo Query usage with apollo-boost,@apollo/react-hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {ApolloProvider} from "@apollo/react-hooks";
+import client from "./apollo";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  // 이런식으로 ApolloProvider 를 활용하여 전체 앱을 감싸줘야 한다.
+    <ApolloProvider client={client}>
+    <App/>
+</ApolloProvider>, document.getElementById("root"));
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,71 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
-export default () => <h1>Home</h1>
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+//   const { loading, error, data } = useQuery(GET_MOVIES);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {/* 이 코드가 loading 상태인 경우 Loading 화면을 보여주게 하는 코드! */}
+      {/* {loading && <Loading>Loading...</Loading>}  */}
+
+      {/* 이런식으로 && 를 여러개 붙이면, 앞에 적힌 두 조건이 true 인 경우 마지막(세번째) 코드를 실행하라는 의미이다 */}
+      {!loading &&
+        data.movies &&
+        data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
+};
